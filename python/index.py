72d09b7db48833941bac4fe35777265c37ea4bc0
mmykhaylov <mikemikhailov2003@gmail.com>
2021-07-04 02:11:06 +0300

    first commit

---
+++ python/index.py
@@ -0,0 +1,26 @@
+from PIL import Image, ImageDraw
+import math
+
+final_image = Image.new("L", (200, 66))
+final_image_drawer = ImageDraw.Draw(final_image)
+
+art = open("art.txt", "r")
+Lines = art.readlines()
+
+colors = {}
+
+characters = "@%&#*/(,. "
+
+for i in range(len(characters)):
+    color_value = math.floor(255 - (255 * (len(characters) - i) / len(characters)))
+    colors[characters[i]] = color_value
+
+for i in range(len(Lines)):
+    line = Lines[i]
+    for j in range(len(line) - 1):
+        char = line[j]
+        final_image_drawer.point([j, i], colors[char])
+
+# final_image.show()
+
+final_image.save('./image.png')
